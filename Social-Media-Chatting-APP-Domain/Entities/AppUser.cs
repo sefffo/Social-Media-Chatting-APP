397bf37399a6b3cf74088f22a84a092c6c696fdc
pc <pc@DESKTOP-2N68CP9>
2026-05-02 20:24:18 +0300

    Fix(AppUser.cs) : fix some naming convenions and adding the nav prop for multiple

--- Social-Media-Chatting-APP-Domain/Entities/AppUser.cs
+++ Social-Media-Chatting-APP-Domain/Entities/AppUser.cs
@@ -4,22 +4,29 @@ namespace Social_Media_Chatting_APP_Domain.Entities
 {
     public class AppUser : IdentityUser
     {
-        public string DisplayName { get; set; }
+        public string? DisplayName { get; set; }
         
-        public string ProfilePicture { get; set; }
+        public string? ProfilePicture { get; set; }
         
-        public string BIO { get; set; }
+        public string? BIO { get; set; }
         
         public bool IsTwoFactorSetup { get; set; }
         
         public bool IsGoogleAccount  { get; set; }
         
-        public bool isOnline { get; set; }
+        public bool IsOnline { get; set; }
         
         
         public DateTime CreatedAt { get; set; }
         
         public DateTime? LastSeen  { get; set; }
         
+        
+        //nav prop 
+        
+        /// <summary>
+        /// As we are implementing multi session so a use can have multuple refreshtokens  
+        /// </summary>
+        public ICollection<RefreshToken> RefreshTokens { get; set; }  
     }
 }
