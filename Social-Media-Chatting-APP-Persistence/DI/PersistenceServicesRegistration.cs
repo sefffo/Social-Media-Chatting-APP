81533c87796105b32821364fde828f64df47abf2
Saif Lotfy <154231473+sefffo@users.noreply.github.com>
2026-05-07 15:48:55 +0300

    fix: add AddRoles<IdentityRole> to Identity registration to support GetRolesAsync

--- Social-Media-Chatting-APP-Persistence/DI/PersistenceServicesRegistration.cs
+++ Social-Media-Chatting-APP-Persistence/DI/PersistenceServicesRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Social_Media_Chatting_APP_Domain.Entities;
 using Social_Media_Chatting_APP_Domain.Interfaces;
 using Social_Media_Chatting_APP_Persistence.DbContext;
@@ -10,20 +11,21 @@ namespace Social_Media_Chatting_APP_Persistence.DI
     {
         public static IServiceCollection AddPersistenceServicesRegistration(this IServiceCollection services)
         {
-
             services.AddIdentityCore<AppUser>(options =>
             {
                 options.Password.RequireDigit = true;
                 options.Password.RequiredLength = 8;
                 options.Password.RequireNonAlphanumeric = false;
-            }).AddEntityFrameworkStores<Social_Media_Chatting_APP_DbContext>();
-            
+            })
+            // AddRoles registers IUserRoleStore and IRoleStore so that
+            // userManager.GetRolesAsync() works. Without this, calling
+            // GetRolesAsync throws NotSupportedException at runtime.
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<Social_Media_Chatting_APP_DbContext>();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
-
-
         }
     }
 }
