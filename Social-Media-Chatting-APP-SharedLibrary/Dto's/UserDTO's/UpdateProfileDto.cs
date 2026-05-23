3512fb1ade458a388a0c8693f268c50ff16a0c9a
Saif Lotfy <154231473+sefffo@users.noreply.github.com>
2026-05-23 11:06:50 +0300

    feat: add PhoneNumber to RegisterDto and UpdateProfileDto

---
+++ Social-Media-Chatting-APP-SharedLibrary/Dto's/UserDTO's/UpdateProfileDto.cs
@@ -0,0 +1,13 @@
+namespace Social_Media_Chatting_APP_SharedLibrary.Dto_s.UserDTO_s;
+
+public class UpdateProfileDto
+{
+    public string? DisplayName { get; set; }
+    public string? Bio { get; set; }
+    public string? Website { get; set; }
+    public string? Location { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool? ShowOnlineStatus { get; set; }
+    public bool? ShowLastSeen { get; set; }
+    public bool? AllowMessageFromStrangers { get; set; }
+}
