d1ed52036efaff9fcc1242dda7e04144011fcfe3
pc <pc@DESKTOP-2N68CP9>
2026-06-21 13:44:33 +0300

    feat(commands): add RespondToFriendRequest command and handler
    - Introduced `RespondToFriendRequestCommand` for handling responses to friend requests.
    - Implemented `RespondToFriendRequestCommandHandler` with logic for validation, permission checks, and status updates.

---
+++ Social-Media-Chatting-APP-Service/Features/Friendship/Commands/FriendRequest/RespondToFriendRequest/RespondToFriendRequestCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Social_Media_Chatting_APP_Domain.Entities;
+using Social_Media_Chatting_APP_Domain.Interfaces;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.FriendshipDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Friendship.Commands.FriendRequest.RespondToFriendRequest;
+
+public class RespondToFriendRequestCommandHandler(
+    IUnitOfWork unitOfWork,
+    IMapper mapper
+) : IRequestHandler<RespondToFriendRequestCommand, Result<FriendshipActionResultDto>>
+{
+    public async Task<Result<FriendshipActionResultDto>> Handle(RespondToFriendRequestCommand request,
+        CancellationToken cancellationToken)
+    {
+        var repo = unitOfWork.GetRepository<Social_Media_Chatting_APP_Domain.Entities.Friendship, Guid>();
+        var friendship = await repo.GetByIdAsync(request.FriendshipId);
+        if (friendship is null)
+        {
+            return Error.NotFound("Friendship.NotFound", "No Friend Request was found.");
+        }
+
+        // check if the user is the one who sent the request and can't accept the ones he sent
+        if (friendship.AddresseeId != request.CurrentUserId)
+        {
+            return Error.Forbidden("Friendship.Forbidden", "You can't accept requests you sent.");
+        }
+
+        if (friendship.Status != FriendshipStatus.Pending)
+        {
+            return Error.BadRequest("Friendship.BadRequest", "Friendship status is not pending.");
+        }
+
+        friendship.Status = Enum.Parse<FriendshipStatus>(request.Decision);
+        friendship.UpdatedAt = DateTime.UtcNow;
+        friendship.CreatedAt = DateTime.UtcNow;
+        repo.Update(friendship);
+        await unitOfWork.SaveChangesAsync();
+        var mappedResult = mapper.Map<FriendshipActionResultDto>(friendship);
+        return Result<FriendshipActionResultDto>.Ok(mappedResult);
+    }
+}
