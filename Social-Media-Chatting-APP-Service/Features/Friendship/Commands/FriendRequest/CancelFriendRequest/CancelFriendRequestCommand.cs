8c17828b4b99ab8d0c1973495e3111291d39e933
pc <pc@DESKTOP-2N68CP9>
2026-06-21 14:16:22 +0300

    feat(commands): add CancelFriendRequest command and handler
    - Introduced `CancelFriendRequestCommand` to handle friend request cancellations.
    - Implemented `CancelFriendRequestCommandHandler` with validation for ownership, pending status, and appropriate error handling.
    - Integrated repository logic to remove friendship entity and save changes.

---
+++ Social-Media-Chatting-APP-Service/Features/Friendship/Commands/FriendRequest/CancelFriendRequest/CancelFriendRequestCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.FriendshipDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Friendship.Commands.FriendRequest.CancelFriendRequest;
+
+public record CancelFriendRequestCommand(Guid CurrentUserId, Guid FriendshipId)
+    : IRequest<Result<FriendshipActionResultDto>>;
---
+++ Social-Media-Chatting-APP-Service/Features/Friendship/Commands/FriendRequest/CancelFriendRequest/CancelFriendRequestCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Social_Media_Chatting_APP_Domain.Entities;
+using Social_Media_Chatting_APP_Domain.Interfaces;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.FriendshipDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Friendship.Commands.FriendRequest.CancelFriendRequest;
+
+public class CancelFriendRequestCommandHandler(
+    IUnitOfWork unitOfWork,
+    IMapper mapper
+) : IRequestHandler<CancelFriendRequestCommand, Result<FriendshipActionResultDto>>
+{
+    private IRequestHandler<CancelFriendRequestCommand, Result<FriendshipActionResultDto>>
+        _requestHandlerImplementation;
+
+    public async Task<Result<FriendshipActionResultDto>> Handle(CancelFriendRequestCommand request,
+        CancellationToken cancellationToken)
+    {
+        var repo = unitOfWork.GetRepository<Social_Media_Chatting_APP_Domain.Entities.Friendship, Guid>();
+        var friendship = await repo.GetByIdAsync(request.FriendshipId);
+        if (friendship is null) return Error.NotFound("Friendship.NotFound", "No friend request was sent");
+        if (friendship.RequestId != request.CurrentUserId)
+            return Error.Forbidden("Friendship.Forbidden", "You can't Cancel this Request");
+        if (friendship.Status != FriendshipStatus.Pending)
+        {
+            return Error.BadRequest("Friendship.BadRequest", "The Addressee Already Took action in Your request");
+        }
+        repo.Remove(friendship);
+        await unitOfWork.SaveChangesAsync();
+        var mappedResult = mapper.Map<FriendshipActionResultDto>(friendship);
+        return Result<FriendshipActionResultDto>.Ok(mappedResult);
+    }
+}
