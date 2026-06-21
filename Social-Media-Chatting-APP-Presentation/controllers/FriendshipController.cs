342aa4902b38adf9771a5d86a002672f2cc81766
saiflotfy <saiflotfy26@gmail.com>
2026-06-21 21:38:41 +0300

    Extend FriendshipController with blocking, unblocking, friendship management, and friend request retrieval endpoints

--- Social-Media-Chatting-APP-Presentation/controllers/FriendshipController.cs
+++ Social-Media-Chatting-APP-Presentation/controllers/FriendshipController.cs
@@ -3,8 +3,14 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Social_Media_Chatting_APP_Presentation.Filters;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Blocking.BlockUser;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Blocking.UnblockUser;
 using Social_Media_Chatting_APP_Service.Features.Friendship.Commands.FriendRequest.RespondToFriendRequest;
 using Social_Media_Chatting_APP_Service.Features.Friendship.Commands.FriendRequest.SendFriendRequest;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Commands.MangeFriendship;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Queries.BlockedUser;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Queries.Friends;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Queries.PenddingRequests;
 using Social_Media_Chatting_APP_SharedLibrary.Dto_s.FriendshipDTO_s;
 
 namespace Social_Media_Chatting_APP_Presentation.Controllers;
@@ -25,13 +31,100 @@ public class FriendshipController(ISender sender) : ApiBaseController
 
     [HttpPut("respond/{friendshipId}")]
     [Authorize]
-    [RedisCacheInvalidate("friendship-requests")]
+    [RedisCacheInvalidateWithResponse("friendship-requests")]
     public async Task<ActionResult<FriendshipActionResultDto>> RespondToFriendRequest(
         Guid friendshipId,
         [FromBody] RespondToFriendRequestDto dto)
     {
         var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-        var result = await sender.Send(new RespondToFriendRequestCommand(userId, friendshipId, dto.Decision));
+        var result = await sender.Send(
+            new RespondToFriendRequestCommand(userId, friendshipId, dto.Decision));
         return HandleResult(result);
+    }
+
+    [HttpDelete("unfriend/{targetUserId}")]
+    [Authorize]
+    [RedisCacheInvalidate("friendship-friends", "targetUserId")]
+    public async Task<ActionResult<FriendshipActionResultDto>> UnfriendUser(Guid targetUserId)
+    {
+        var UserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var result = await sender.Send(new UnfriendCommand(UserId, targetUserId));
+        return HandleResult(result);
+    }
+
+    [HttpPost("block/{targetUserId}")]
+    [RedisCacheInvalidate("friendship-friends", "targetUserId")]
+    [RedisCacheInvalidate("friendship-requests", "targetUserId")]
+    [Authorize]
+    public async Task<ActionResult<FriendshipActionResultDto>> BlockUser(Guid targetUserId)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var Result = await sender.Send(new BlockUserCommand(userId, targetUserId));
+        return HandleResult(Result);
+    }
+
+    [HttpDelete("unblock/{targetUserId}")]
+    [RedisCacheInvalidate("friendship-blocked", "targetUserId")] //to check the block json cached 
+    [Authorize]
+    public async Task<ActionResult<FriendshipActionResultDto>> UnblockUser(Guid targetUserId)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var Result = await sender.Send(new UnblockUserCommand(userId, targetUserId));
+        return HandleResult(Result);
+    }
+
+    [HttpGet("friends")]
+    [RedisCache("friendship-friends", ttlSeconds: 1000)]
+    [Authorize]
+    public async Task<ActionResult<IEnumerable<FriendListItemDto>>> GetFriends()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var result = await sender.Send(new GetFriendsQuery(userId));
+        return HandleResult(result);
+    }
+
+    [HttpGet("blocked")]
+    [Authorize]
+    [RedisCache("friendship-blocked", ttlSeconds: 1000)]
+    public async Task<ActionResult<IEnumerable<BlockedUserItemDto>>> GetBlocked()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var result = await sender.Send(new GetBlockedUsersQuery(userId));
+        return HandleResult(result);
+    }
+
+    [HttpGet("requests/incoming")]
+    [Authorize]
+    [RedisCache("friendship-requests-incoming", ttlSeconds: 300)]
+    public async Task<ActionResult<FriendshipActionResultDto>> GetFriendRequests()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var result = await sender.Send(new PendingRequestsQuery(userId));
+
+
+        if (!result.IsSuccess)
+            return HandleResult(result);
+
+        var incomingResult = result.GetValueOrThrow().Where(i => i.Direction == "Incoming"
+        );
+
+        return Ok(incomingResult);
+    }
+
+    [HttpGet("requests/outgoing")]
+    [Authorize]
+    [RedisCache("friendship-requests-outgoing", ttlSeconds: 300)]
+    public async Task<ActionResult<IEnumerable<FriendRequestItemDto>>> GetOutgoingRequests()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        var result = await sender.Send(new PendingRequestsQuery(userId));
+
+        if (!result.IsSuccess)
+            return HandleResult(result);
+
+        var outgoing = result.GetValueOrThrow()
+            .Where(r => r.Direction == "Outgoing");
+
+        return Ok(outgoing);
     }
 }
