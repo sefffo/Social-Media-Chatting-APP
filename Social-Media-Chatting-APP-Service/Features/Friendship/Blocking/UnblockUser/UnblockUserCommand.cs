1eba26f9c405f2ba07601841eedc03de0dbc6c1a
pc <pc@DESKTOP-2N68CP9>
2026-06-21 15:17:52 +0300

    feat(commands): add UnblockUser command and handler
    - Implemented `UnblockUserCommand` and `UnblockUserCommandHandler` for unblocking users.
    - Added validation to ensure the relationship exists, is blocked, and the current user initiated the block.
    - Integrated repository logic to remove the blocked relationship and persist changes.

---
+++ Social-Media-Chatting-APP-Service/Features/Friendship/Blocking/UnblockUser/UnblockUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.FriendshipDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Friendship.Blocking.UnblockUser;
+
+public record UnblockUserCommand(Guid CurrentUser, Guid TargetedUserId) : IRequest<Result<FriendshipActionResultDto>>;
---
+++ Social-Media-Chatting-APP-Service/Features/Friendship/Blocking/UnblockUser/UnblockUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Social_Media_Chatting_APP_Domain.Entities;
+using Social_Media_Chatting_APP_Domain.Interfaces;
+using Social_Media_Chatting_APP_Service.Features.Friendship.Helpers;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.FriendshipDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Friendship.Blocking.UnblockUser;
+
+public class UnblockUserCommandHandler(
+    IUnitOfWork unitOfWork,
+    IMapper mapper
+) : IRequestHandler<UnblockUserCommand, Result<FriendshipActionResultDto>>
+{
+    public async Task<Result<FriendshipActionResultDto>> Handle(UnblockUserCommand request,
+        CancellationToken cancellationToken)
+
+    {
+        var repo = unitOfWork.GetRepository<Social_Media_Chatting_APP_Domain.Entities.Friendship, Guid>();
+
+        var existing = await FriendshipQueryHelper.GetAsync(repo, request.CurrentUser, request.TargetedUserId);
+
+
+        if (existing is null)
+            return Error.NotFound("Friendship.NotFound", "No relationship found with this user.");
+
+        if (existing.Status != FriendshipStatus.Blocked)
+        {
+            return Error.BadRequest("Unblock.NotBlocked", "You cant unblock this user as you didn't block him/her");
+        }
+
+        if (existing.BlockedById != request.CurrentUser)
+        {
+            return Error.Forbidden("Friendship.NotBlocker", "You did not block this user");
+        }
+
+
+        repo.Remove(existing);
+        await unitOfWork.SaveChangesAsync();
+        return Result<FriendshipActionResultDto>.Ok(mapper.Map<FriendshipActionResultDto>(existing));
+    }
+}
