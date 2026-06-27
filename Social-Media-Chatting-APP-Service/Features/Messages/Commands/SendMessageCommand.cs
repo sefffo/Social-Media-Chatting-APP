5a34c5920466f787c72b3b5778b580b50a4b6a56
pc <pc@DESKTOP-2N68CP9>
2026-06-27 17:15:16 +0300

    feat(commands): add SendMessageCommand for handling message sending requests
    - Introduced `SendMessageCommand` to support sending messages with text, media, file attachments, and reply functionality.

---
+++ Social-Media-Chatting-APP-Service/Features/Messages/Commands/SendMessageCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Social_Media_Chatting_APP_Domain.Entities.Enums;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.MessagesDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Messages.Commands;
+
+public record SendMessageCommand(
+    Guid senderId, // from the JWT Token 
+    Guid ConversationId,
+    MessageContentType contentType,
+    string? textContent,
+    string? MediaUrl,
+    string? FileName,
+    Guid? ReplyToMessageId
+)
+    : IRequest<Result<MessageDto>>;
