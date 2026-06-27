0fa9a86ef230351464417412355369980af45f7c
pc <pc@DESKTOP-2N68CP9>
2026-06-27 03:28:37 +0300

    feat(specifications): add MessagesSpecifications for message filtering and pagination
    - Implemented `MessagesSpecifications` to support querying messages by conversation ID and deletion status.
    - Added support for including sender and read statuses, ordering by sent date, and pagination.

---
+++ Social-Media-Chatting-APP-Service/Specification/Messages/MessagesSpecifications.cs
@@ -0,0 +1,16 @@
+﻿using Social_Media_Chatting_APP_Domain.Entities;
+using Social_Media_Chatting_APP_Domain.Specifications;
+
+namespace Social_Media_Chatting_APP_Service.Specification.Messages;
+
+public class MessagesSpecifications : BaseSpecification<Message>
+{
+    public MessagesSpecifications(Guid conversationId, int pageIndex, int pageSize) : base(m =>
+        m.ConversationId == conversationId && !m.IsDeleted)
+    {
+        AddIncludes(m => m.Sender);
+        AddIncludes(m => m.ReadStatuses);
+        ApplyOrderByDescending(m => m.SentAt);
+        ApplyPagination(pageSize, pageIndex);
+    }
+}
