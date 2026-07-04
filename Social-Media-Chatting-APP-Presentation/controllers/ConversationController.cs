54f11dfeb8c6508de15845e1185dc33533504574
pc <pc@DESKTOP-2N68CP9>
2026-07-04 18:17:50 +0300

    feat(controllers): add `MessageController` and `ConversationController` APIs
    - Introduced `MessageController` with initial setup.
    - Added `ConversationController` featuring a `GetConversations` endpoint to fetch paginated user conversations.

---
+++ Social-Media-Chatting-APP-Presentation/controllers/ConversationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Social_Media_Chatting_APP_Service.Features.Conversations.Queries.Conversations;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.ConversationDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConversationController(
+    ISender sender
+) : ApiBaseController
+{
+    [Authorize]
+    [HttpGet("conversations")]
+    public async Task<ActionResult<CursorPaginatedResult<ConversationDto>>> GetConversations([FromQuery] DateTime? before,
+        [FromQuery] int pageSize = 20)
+    {
+        
+        var user =Guid.Parse( User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var result = await sender.Send(new GetConversationsQuery(user,before, pageSize));
+        return HandleResult(result);
+        
+    }
+}
---
+++ Social-Media-Chatting-APP-Presentation/controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Social_Media_Chatting_APP_Presentation.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessageController(
+    ISender sender
+    ) : ApiBaseController
+{
+  
+    
+    
+}
