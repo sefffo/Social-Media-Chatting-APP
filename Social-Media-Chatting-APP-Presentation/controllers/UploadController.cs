2baaa159cf1da5ea7485ba0fd3aefae69cbba5f5
pc <pc@DESKTOP-2N68CP9>
2026-07-08 22:23:21 +0300

    chore(upload): remove unused `Upload` namespace from `UploadController`

--- Social-Media-Chatting-APP-Presentation/controllers/UploadController.cs
+++ Social-Media-Chatting-APP-Presentation/controllers/UploadController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Social_Media_Chatting_APP_Domain.Entities.Enums;
-using Social_Media_Chatting_APP_Service.Common.Upload;
 using Social_Media_Chatting_APP_ServiceAbstraction;
 using Social_Media_Chatting_APP_SharedLibrary.Dto_s.CloudinaryDTO_s;
 using Social_Media_Chatting_APP_SharedLibrary.Enums;
