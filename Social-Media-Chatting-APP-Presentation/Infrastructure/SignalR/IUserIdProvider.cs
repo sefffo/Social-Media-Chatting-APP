3eb5070e8889fbab3dc16a59288653f216a0df89
pc <pc@DESKTOP-2N68CP9>
2026-07-18 17:04:54 +0300

    feat(signalr): add `IUserIdProvider` interface and `UserIdProvider` implementation placeholder
    - Introduced `IUserIdProvider` interface to define user ID resolution for SignalR connections.
    - Added `UserIdProvider` class with a placeholder `GetUserIdAsync` method for future implementation.

---
+++ Social-Media-Chatting-APP-Presentation/Infrastructure/SignalR/IUserIdProvider.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Social_Media_Chatting_APP_Presentation.Infrastructure.SignalR;
+
+public interface IUserIdProvider
+{
+    public Task<string> GetUserIdAsync(HubConnectionContext connection);
+}
---
+++ Social-Media-Chatting-APP-Presentation/Infrastructure/SignalR/UserIdProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Social_Media_Chatting_APP_Presentation.Infrastructure.SignalR;
+
+public class UserIdProvider : IUserIdProvider
+{
+    public Task<string> GetUserIdAsync(HubConnectionContext connection)
+    {   
+        throw new NotImplementedException();
+    }
+}
